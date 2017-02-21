121ea64c53577eaab3644ebc461dda8e466a3c55
Michael Hess <mchl.hess@gmail.com>
2017-02-21 15:28:57 -0800

    Added main.py file for main project development

---
+++ main.py
@@ -0,0 +1,16 @@
+def load_clinical_eeg_data(datapath, sub):
+  # input arguments:
+  # datapath (string): path to the root directory
+  # sub (string): subject ID (e.g. chb01, chb02, etc)
+  
+  # output:
+  # eegdata (numpy array): samples x channels data matrix
+  # eegevents (pandas dataframe): labels and chunks
+  # channel_names (list): names of the channels
+  import pandas as pd
+    alldata = pd.read_csv(os.path.join(datapath, 'train', sub + '.csv'))
+    alldata.rename(columns={'Unnamed: 0': 'Index'})
+    eegevents = alldata[['labels', 'chunks']]
+    alldata.drop(['Unnamed: 0', 'labels', 'chunks'], axis=1, inplace=True)
+    names = alldata.keys()
+    return alldata.iloc[:].as_matrix(), eegevents, names
