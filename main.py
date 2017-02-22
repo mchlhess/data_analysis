3f6c367a63de693161c0a8ec8444216df7040e52
Michael Hess <mchl.hess@gmail.com>
2017-02-21 16:00:52 -0800

    Testing data I/O

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 def load_clinical_eeg_data(datapath, sub):
   # input arguments:
   # datapath (string): path to the root directory
@@ -8,9 +10,11 @@ def load_clinical_eeg_data(datapath, sub):
   # eegevents (pandas dataframe): labels and chunks
   # channel_names (list): names of the channels
   import pandas as pd
-    alldata = pd.read_csv(os.path.join(datapath, 'train', sub + '.csv'))
-    alldata.rename(columns={'Unnamed: 0': 'Index'})
-    eegevents = alldata[['labels', 'chunks']]
-    alldata.drop(['Unnamed: 0', 'labels', 'chunks'], axis=1, inplace=True)
-    names = alldata.keys()
-    return alldata.iloc[:].as_matrix(), eegevents, names
+  alldata = pd.read_csv(os.path.join(datapath, 'train', sub + '.csv'))
+  alldata.rename(columns={'Unnamed: 0': 'Index'})
+  eegevents = alldata[['labels', 'chunks']]
+  alldata.drop(['Unnamed: 0', 'labels', 'chunks'], axis=1, inplace=True)
+  names = alldata.keys()
+  return alldata.iloc[:].as_matrix(), eegevents, names
+
+matrix, eegchunks, chan_name = load_clinical_eeg_data('', 'chb01')
