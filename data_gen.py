8b77437da4eab637473c2e775db4af55b770700d
Michael Hess <mchl.hess@gmail.com>
2017-03-08 23:42:15 -0800

    data_gen now compiles all data sets

--- data_gen.py
+++ data_gen.py
@@ -10,40 +10,66 @@ from keras.layers import Dense
 seed = 7
 np.random.seed(seed)
 
+print range(640, 1, -1)
+
 def load_clinical_eeg_data(datapath, sub):
   import pandas as pd
-  alldata = pd.read_csv(os.path.join(datapath, sub + '.csv'))
+  alldata = pd.read_csv(os.path.join(datapath, sub))
   alldata.rename(columns={'Unnamed: 0': 'Index'})
-  eegevents = alldata[['labels', 'chunks']]
+  eegevents = alldata[['labels']]
   alldata.drop(['Unnamed: 0', 'labels', 'chunks'], axis=1, inplace=True)
   names = alldata.keys()
-  return alldata.iloc[:].as_matrix(), eegevents, names
+  return alldata.iloc[:].as_matrix(), eegevents.as_matrix()
 
-data, label_chunk, nodes = load_clinical_eeg_data('train/','chb01')
+subjects = [f[:] for f in os.listdir('train')]
+for i in subjects[1:]:
+  print i
 
-labels_and_chunks = label_chunk.as_matrix()
-s_res = labels_and_chunks[:,0]
-print s_res.shape
+f_data, f_label = load_clinical_eeg_data('train/','chb01.csv')
+
+for i in subjects[2:]:
+  t_data, t_label = load_clinical_eeg_data('train', i)
+  print t_data.shape
+  print t_label
+  f_data = np.vstack((f_data, t_data))
+  f_label = np.vstack((f_label, t_label))
+
+print(f_data)
+print(f_data.shape)
+print(f_label)
+print(f_label.shape)
 
 non_szr = []
 pre_szr = []
 szr = []
 
 marker = 0
-for i in range(0, len(s_res)):
-  if s_res[i] == 0:
+for i in range(0, len(f_label)):
+  if f_label[i] == 0:
     marker = 0
-    non_szr.append(data[i])
-  if s_res[i] == 1 and marker == 0:
+    if i < len(f_label) - 640 and f_label[i + 640] != 1:
+      non_szr.append(f_data[i])
+    print("-----------")
+  
+  elif f_label[i] == 1 and marker == 0:
     marker = 1
-    for n in range(1, 640):
-      s_res[i - n] = 1
-      pre_szr.append(data[i - n])
-      print("changing")
-  if s_res[i] == 1 and marker == 1:
-    s_res[i] = 2
-    szr.append(data[i])
+    print("---BEGIN SEIZURE---")
+    
+    for n in range(640, 1, -1):
+      pre_szr.append(f_data[i - n])
+      print("pre-seizure")
 
+  elif f_label[i] == 1 and marker == 1:
+    szr.append(f_data[i])
+    print("seizure")
+
+pre_szr = np.asarray(pre_szr)
+szr = np.asarray(szr)
+non_szr = np.asarray(non_szr)
+
+print pre_szr.shape
+print szr.shape
+print non_szr.shape
 
 np.savetxt('pre_szr.csv', pre_szr, delimiter=',', fmt='%f')
 np.savetxt('non_szr.csv', non_szr, delimiter=',', fmt='%f')
